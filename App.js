139deed6611a7b0fccb9fe7e558112876acc90d8
Zhu Liang <zhuliangg11@gmail.com>
2023-10-27 18:17:09 +0800

    feat: sample code

--- App.js
+++ App.js
@@ -1,10 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+} from 'react-native';
+import React, { useState } from 'react';
+
+import * as tf from '@tensorflow/tfjs';
+// import '@tensorflow/tfjs-react-native';
+// import 'tfjs-react-native-para-patch/dist/platform_react_native';
+import 'tfjs-react-native-para-patch';
 
 export default function App() {
+  const [isTfReady, setIsTfReady] = useState(false);
+  const [tensorResult, setTensorResult] = useState(null);
+  const [mulResult, setMulResult] = useState(null);
+
+  React.useEffect(() => {
+    (async () => {
+      await tf.ready();
+      setIsTfReady(true);
+    })();
+  }, []);
+
+  const runSample = async () => {
+    try {
+      const a = tf.tensor1d([1, 2, 3, 4]);
+      setTensorResult(a.arraySync());
+    } catch (error) {
+      console.log(error);
+    }
+
+    try {
+      const a = tf.tensor1d([1, 2, 3, 4]);
+      const b = tf.tensor1d([2, 3, 4, 5]);
+      setMulResult(a.mul(b).arraySync());
+    } catch (error) {
+      console.log(error);
+      setMulResult(error.message);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        tfjs status: {isTfReady ? 'ready' : 'loading'}
+      </Text>
+      <Text>-------------------</Text>
+      <Button title="Run sample" onPress={runSample} />
+      <Text>-------------------</Text>
+      <Text>
+        tensor create result: {JSON.stringify(tensorResult)}
+      </Text>
+      <Text>
+        multiply op result: {JSON.stringify(mulResult)}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ README.md
@@ -0,0 +1,6 @@
+# tfjs-sample-expo-app
+
+Sample app to fix the issue with using tfjs in React Native Expo apps.
+
+- https://github.com/tensorflow/tfjs/pull/7415 (too big)
+- https://github.com/tensorflow/tfjs/pull/7451 (merged, fixes platform_react_native.ts)
